fc6af40cb232fb51fb2ae7888405d4d58d5d1201
Daniel Hart <super.awesome.hartd@gmail.com>
2023-08-23 21:23:13 -0400

    Fixed Bug

--- Program.cs
+++ Program.cs
@@ -79,10 +79,10 @@ class CPU {
   }
 
   // opcodes
-  public const byte INS_LDA_IM = 0xA9;
-  public const byte INS_LDA_ZP = 0xA5;
+  public const byte INS_LDA_IM  = 0xA9;
+  public const byte INS_LDA_ZP  = 0xA5;
   public const byte INS_LDA_ZPX = 0xB5;
-  public const byte INS_JSR = 0x20;
+  public const byte INS_JSR     = 0x20;
 
   public void LDASetStatus() {
     Z = A == 0;
@@ -91,7 +91,9 @@ class CPU {
 
   public void Execute(uint Cycles, Mem memory) {
     while (Cycles > 0) {
+      // Console.Write($"Reading from addr 0x{PC:X4}, ");
       byte Ins = FetchByte(ref Cycles, memory);
+      // Console.WriteLine($"Ins = 0x{Ins:X2}");
       switch (Ins) {
         case INS_LDA_IM: {
           byte Value = FetchByte(ref Cycles, memory);
@@ -115,14 +117,14 @@ class CPU {
         }
         case INS_JSR: {
           ushort SubAddr = FetchWord(ref Cycles, memory);
-          memory.WriteWord((ushort)(PC - 1), SubAddr, ref Cycles);
-          SP++;
+          memory.WriteWord((ushort)(PC - 1), (ushort)(SP - 2), ref Cycles);
+          SP -= 2;
           PC = SubAddr;
           Cycles--;
           break;
         }
         default: {
-          Console.WriteLine("Instruction not handled");
+          Console.WriteLine($"Instruction not handled, recieved instruction {Ins:X2}");
           break;
         }
       }
@@ -139,11 +141,13 @@ class Program {
     cpu.Reset(mem);
 
     // inline a little program
-    mem[0xFFFC] = CPU.INS_LDA_ZP;
+    mem[0xFFFC] = CPU.INS_JSR;
     mem[0xFFFD] = 0x42;
-    mem[0x0042] = 0x84;
+    mem[0xFFFE] = 0x42;
+    mem[0x4242] = CPU.INS_LDA_IM;
+    mem[0x4243] = 0x84;
 
-    cpu.Execute(3, mem);
+    cpu.Execute(8, mem);
 
     // Debug
     Console.WriteLine($"A = 0x{cpu.A:X2}");
